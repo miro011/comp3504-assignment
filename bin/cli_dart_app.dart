cfe0cd1191aaf824c0daac92deecf6d624373cfd
eightys3v3n <eightys3v3n@gmail.com>
2022-10-04 17:53:32 -0600

    Add Android Studio project files

--- bin/cli_dart_app.dart
+++ bin/cli_dart_app.dart
@@ -1,5 +1,8 @@
 import 'package:cli_dart_app/cli_dart_app.dart' as cli_dart_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli_dart_app.calculate()}!');
+  var count = 0;
+  if (count < 10) {
+    print('Hello world: ${cli_dart_app.calculate()}!');
+  }
 }
