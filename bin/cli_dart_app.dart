0dc0226eee513dfc7a8dc1870075ecdd16186433
eightys3v3n <eightys3v3n@gmail.com>
2022-10-05 17:22:49 -0600

    Implement menu structure for user interaction
    Created a menu library that has methods for printing a prompt and
    retrieving user input.

--- bin/cli_dart_app.dart
+++ bin/cli_dart_app.dart
@@ -1,8 +1,28 @@
-import 'package:cli_dart_app/cli_dart_app.dart' as cli_dart_app;
+import 'dart:io';
+import 'package:cli_dart_app/menu.dart' as menu;
+
 
 void main(List<String> arguments) {
-  var count = 0;
-  if (count < 10) {
-    print('Hello world: ${cli_dart_app.calculate()}!');
+  String answer = menu.mainMenu();
+  switch(answer) {
+    case menu.addItemKey: {
+      stdout.writeln("Adding item...");
+      break;
+    }
+    case menu.removeItemKey: {
+      stdout.writeln("Removing item...");
+      break;
+    }
+    case menu.searchItemKey: {
+      stdout.writeln("Searching for item...");
+      break;
+    }
+    case menu.quitItemKey: {
+      stdout.writeln("Quitting...");
+      break;
+    }
+    default: {
+      stderr.writeln("Invalid selection returned for main menu. This only happens if something is implemented in the main menu but not in the switch statement after the main menu.");
+    }
   }
 }
