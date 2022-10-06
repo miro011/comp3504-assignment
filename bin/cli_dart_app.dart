e8cf67412c7463d2c1d3a78ed97e2ca69067f157
tmtkachyk <tmtkachyk@gmail.com>
2022-10-06 13:46:48 -0600

    inventory sentence parsing

--- bin/cli_dart_app.dart
+++ bin/cli_dart_app.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'package:cli_dart_app/item.dart';
 import 'package:cli_dart_app/menu.dart' as menu;
+import 'dart:convert';
 
 void main(List<String> arguments) {
   //itemFile path
@@ -8,7 +10,21 @@ void main(List<String> arguments) {
   //read file and store lines in a List of strings. Is async
 
   itemFile.readAsLines().then((List<String> lines) {
-    print(lines[1]);
+    //delimeter for each setence
+    List words = [];
+    words = lines[1].split(";");
+
+    var itemId = int.parse(words[0]);
+    var name = words[1];
+    var price = double.parse(words[3]);
+    var supplierId = int.parse(words[4]);
+    var quantity = int.parse(words[2]);
+
+    var anItem = Item(itemId, name, price, supplierId, quantity);
+
+    print(anItem);
+    //create item from each line in the array
+    //put items into itemList
 
     //provide action menu
     String answer = menu.mainMenu();
--- lib/item.dart
+++ lib/item.dart
@@ -4,23 +4,20 @@ class Item {
   int quantity;
   int minQuantity;
   int defaultOrderQuantity;
+  double price;
+  int supplierId;
 
-  Item(
-    this.id,
-    this.name,
-    this.quantity,
-    [
-      this.minQuantity=10,
-      this.defaultOrderQuantity=30
-    ]
-  );
+  Item(this.id, this.name, this.price, this.supplierId, this.quantity,
+      [this.minQuantity = 10, this.defaultOrderQuantity = 30]);
 
-@override
-String toString() {
+  @override
+  String toString() {
     return '{'
-      'id: $id, '
-      'name: $name, '
-      'quantity: $quantity'
-      '}';
+        'id: $id, '
+        'name: $name, '
+        'price: $price, '
+        'supplier ID: $supplierId, '
+        'quantity: $quantity'
+        '}';
   }
 }
