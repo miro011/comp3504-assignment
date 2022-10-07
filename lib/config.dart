ec649ba410dc92a3ea93b96abafb2bd1b55dc81a
eightys3v3n <eightys3v3n@gmail.com>
2022-10-06 20:35:58 -0600

    Add resource library to auto-resolve resources folder
    VSCode and AndroidStudio set different working paths when running the 
    main program. This accounts for that so they both still find the 
    resources directory.

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,2 @@
 const csv_separator = ';';
-const suppliers_file_path = 'resources/suppliers.txt';
+const suppliers_file_path = 'resources/suppliers.txt';const resources_dirs = ['resources', '../resources'];
---
+++ lib/resources.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'package:cli_dart_app/config.dart' as config;
+import 'package:path/path.dart' as path;
+
+
+String? resourcesPath;
+
+
+void findResourcesDir() {
+  Directory? resourcesDir;
+
+  if (resourcesPath == null) {
+    for (final dir in config.resources_dirs) {
+      resourcesDir = Directory(dir);
+      if (resourcesDir.existsSync()) {
+        resourcesPath = dir;
+        break;
+      }
+    }
+    if (resourcesPath == null) {
+      throw FileSystemException(
+          "Couldn't find any of the hard-coded resource directories");
+    }
+  }
+}
+
+
+String readAsString(String resourceName) {
+  findResourcesDir();
+
+  File f = File(path.join(resourcesPath as String, resourceName));
+
+  String contents = f.readAsStringSync();
+
+  return contents;
+}
+
+
+List<String> readAsLines(String resourceName) {
+  findResourcesDir();
+
+  File f = File(path.join(resourcesPath as String, resourceName));
+
+  List<String> contents = f.readAsLinesSync();
+
+  return contents;
+}
