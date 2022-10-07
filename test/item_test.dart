8dc378c0f52e489b33fca54d038e78433a5703f5
eightys3v3n <eightys3v3n@gmail.com>
2022-10-07 14:03:17 -0600

    Replace default == behaviour of Item
    Default comparison behaviour checks if the items refer to the same thing
    in memory. This is non-intuitive so it was replaced with checking if
    both objects have the same attribute values.

--- test/item_test.dart
+++ test/item_test.dart
@@ -10,4 +10,69 @@ void main() {
           '3000;Knock Bits;18;12.67;50015');
     });
   });
+  group('ItemList.==', () {
+    test('Equal Objects', () {
+      Item one = Item(
+        3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, two);
+    });
+    test('Equal Objects w/o Optional Parameters', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18);
+      expect(one, two);
+    });
+    test('Unequal Objects by ID', () {
+      Item one = Item(
+          3001, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by Name', () {
+      Item one = Item(
+          3000, 'Knock Bits1', 12.67, 50015, 18, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by Price', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.7, 50015, 18, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by SupplierID', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.67, 50016, 18, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by Quantity', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.67, 50015, 19, 10, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by MinQuantity', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 11, 30);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+    test('Unequal Objects by DefaultOrderQuantity', () {
+      Item one = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 31);
+      Item two = Item(
+          3000, 'Knock Bits', 12.67, 50015, 18, 10, 30);
+      expect(one, isNot(equals(two)));
+    });
+  });
 }
