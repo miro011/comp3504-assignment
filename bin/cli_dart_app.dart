b67c61330945700d72825d7496fa14477da70cdb
tmtkachyk <tmtkachyk@gmail.com>
2022-10-07 10:49:57 -0600

    items from resource

--- bin/cli_dart_app.dart
+++ bin/cli_dart_app.dart
@@ -4,28 +4,17 @@ import 'package:cli_dart_app/item.dart';
 import 'package:cli_dart_app/menu.dart' as menu;
 import 'package:cli_dart_app/supplier_list.dart';
 import 'package:cli_dart_app/resources.dart' as resources;
-
+import 'package:cli_dart_app/itemList.dart';
 
 void main(List<String> arguments) {
-  List<String> lines = resources.readAsLines(config.items_resource_name);
+  //Creates a ItemList of Item objects from resource config files
+  ItemList items = ItemList.addResourceItems(config.items_resource_name);
 
-  //delimeter for each setence
-  List words = [];
-  words = lines[1].split(";");
+  //print(items.getItem(1).name);
 
-  var itemId = int.parse(words[0]);
-  var name = words[1];
-  var price = double.parse(words[3]);
-  var supplierId = int.parse(words[4]);
-  var quantity = int.parse(words[2]);
-
-  var anItem = Item(itemId, name, price, supplierId, quantity);
-
-  print(anItem);
-  //create item from each line in the array
-  //put items into itemList
-
-  SupplierList suppliers = SupplierList.fromResource(config.suppliers_resource_name);
+  //Creates a SupplierList of Supplioer objects from resource config files
+  SupplierList suppliers =
+      SupplierList.fromResource(config.suppliers_resource_name);
 
   //provide action menu
   String answer = menu.mainMenu();
