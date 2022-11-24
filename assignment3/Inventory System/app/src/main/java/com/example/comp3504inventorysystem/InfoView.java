a498803a2a419496f606272944513ae0806c5e75
Lorenzo Young <lyoun3066@mtroyal.ca>
2022-11-24 06:01:10 -0700

    Create Main menu screen. Working on Add Item screen

---
+++ assignment3/Inventory System/app/src/main/java/com/example/comp3504inventorysystem/InfoView.java
@@ -0,0 +1,14 @@
+package com.example.comp3504inventorysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_view);
+    }
+}
