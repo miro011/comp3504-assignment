ab7630a43ec7338379bc485e0c82490bbf20c67e
Lorenzo Young <lyoun306@mytroyal.ca>
2022-12-03 03:27:44 -0700

    Beautified the List Item class and also allow scrolling when the list is too many for the screen.

--- assignment3/Inventory System/app/src/main/java/com/example/comp3504inventorysystem/ListItemActivity.java
+++ assignment3/Inventory System/app/src/main/java/com/example/comp3504inventorysystem/ListItemActivity.java
@@ -1,12 +1,16 @@
 package com.example.comp3504inventorysystem;
 
-import android.graphics.drawable.GradientDrawable;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.LinearLayout;
+
 import com.example.comp3504inventorysystem.databinding.ActivityListItemViewBinding;
+
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 
@@ -41,13 +45,69 @@ public class ListItemActivity extends DrawerBaseActivity {
 
         // Display
 
-        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.listItem_output);
+//        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.listItem_output);
+//
+//        for (Item item : matchedItems) {
+//            TextView textView = new TextView(this);
+//            textView.setText(item.toString());
+//            linearLayout.addView(textView);
+//        }
+
+        TableLayout linearLayout = findViewById(R.id.table_output);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10,5,10,5);
+
+
+        TableRow tbHeader = (TableRow)findViewById(R.id.table_header);
+        tbHeader.setGravity(Gravity.CENTER);
+        Typeface font = getResources().getFont(R.font.acme);
+
 
         for (Item item : matchedItems) {
-            TextView textView = new TextView(this);
-            textView.setText(item.toString());
-            linearLayout.addView(textView);
+
+            TableRow tbr = new TableRow(this);
+            tbr.setGravity(Gravity.CENTER);
+            TextView idView = new TextView(this);
+            TextView nameView = new TextView(this);
+            TextView quantityView = new TextView(this);
+            TextView priceView = new TextView(this);
+            TextView supplyIdView = new TextView(this);
+
+            idView.setLayoutParams(params);
+            nameView.setLayoutParams(params);
+            quantityView.setLayoutParams(params);
+            priceView.setLayoutParams(params);
+            supplyIdView.setLayoutParams(params);
+
+            idView.setGravity(Gravity.CENTER | Gravity.CENTER);
+            nameView.setGravity(Gravity.CENTER | Gravity.CENTER);
+            quantityView.setGravity(Gravity.CENTER | Gravity.CENTER);
+            priceView.setGravity(Gravity.CENTER | Gravity.CENTER);
+            supplyIdView.setGravity(Gravity.CENTER | Gravity.CENTER);
+
+            idView.setTypeface(font);
+            nameView.setTypeface(font);
+            quantityView.setTypeface(font);
+            priceView.setTypeface(font);
+            supplyIdView.setTypeface(font);
+
+
+
+            idView.setText(String.valueOf(item.getId()));
+            nameView.setText(item.getName());
+            quantityView.setText(String.valueOf(item.getQuantity()));
+            priceView.setText(String.valueOf(item.getPrice()));
+            supplyIdView.setText(String.valueOf(item.getSupplierId()));
+            tbr.addView(idView);
+            tbr.addView(nameView);
+            tbr.addView(quantityView);
+            tbr.addView(priceView);
+            tbr.addView(supplyIdView);
+
+            linearLayout.addView(tbr);
+
         }
+
     }
 
     @Override
